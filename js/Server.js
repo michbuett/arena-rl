f41b1042a335b485428892a2a2c286ab39e161cc
Michael Buettner <michbuett@gmx.de>
2013-05-20 11:57:23 +0200

    Add static file server

---
+++ js/Server.js
@@ -0,0 +1,9 @@
+﻿var connect = require('connect');
+var http = require('http');
+var rootPath = __dirname + '/../';
+
+var app = connect()
+  .use(connect.static(rootPath))
+  .use(connect.directory(rootPath));
+
+http.createServer(app).listen(3000);
